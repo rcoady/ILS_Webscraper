698801d0738d87df1da5e141c828183bed14e4ef
Ryan Coady <rcoady91@gmail.com>
2015-10-20 17:30:09 -0400

    Searches website to populate the county_list, reworks schoolsearch to better populate the school_list, as well as reworking the savetofile so that the data from school_list is printed to two columns

--- highschool.py
+++ highschool.py
@@ -2,29 +2,24 @@ from bs4 import BeautifulSoup
 import csv
 import requests
 
+# Website that is being scraped
 url = "https://en.wikipedia.org/wiki/List_of_high_schools_in_Illinois"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
+county_list = []
 school_list = []
 
-# List of All counties in Illinois ##
-# There has to be an easier way of doing this ##
-county_list = ['Adams', 'Alexander', 'Bond', 'Boone', 'Brown', 'Bureau', 'Calhoun',
-               'Carroll', 'Cass', 'Champaign', 'Christian', 'Clark', 'Clay', 'Clinton', 'Coles', 'Cook',
-               'Crawford', 'Cumberland', 'DeKalb', 'DeWitt', 'Douglas', 'DuPage', 'Edgar', 'Edwards',
-               'Effingham', 'Fayette', 'Ford', 'Franklin', 'Fulton', 'Gallatin', 'Greene', 'Grundy',
-               'Hamilton', 'Hancock', 'Hardin', 'Henderson', 'Henry', 'Iroquois', 'Jackson', 'Jasper',
-               'Jefferson', 'Jersey', 'Jo_Daviess', 'Johnson', 'Kane', 'Kankakee', 'Kendall', 'Knox',
-               'LaSalle', 'Lake', 'Lawrence', 'Lee', 'Livingston', 'Logan', 'Macon', 'Macoupin',
-               'Madison', 'Marion', 'Marshall', 'Mason', 'Massac', 'McDonough', 'McHenry', 'McLean',
-               'Menard', 'Mercer', 'Monroe', 'Montgomery', 'Morgan', 'Ogle', 'Peoria', 'Perry', 'Piatt',
-               'Pike', 'Pope', 'Pulaski', 'Putnam', 'Randolph', 'Richland', 'Rock_Island', 'St._Clair',
-               'Saline', 'Sangamon', 'Schuyler', 'Scott', 'Shelby', 'Stark', 'Stephenson', 'Tazewell',
-               'Union', 'Vermilion', 'Wabash', 'Warren', 'Washington', 'Wayne', 'White', 'Whiteside',
-               'Will', 'Williamson', 'Winnebago', 'Woodford']
 
-# Appends _County to the end of each element of the county list
-county_list = [element + '_County' for element in county_list]
+# Goes through the page and pulls out all of the county Id's
+
+
+def countysearch():
+    for tag in soup.findAll('span', id =lambda x: x and x.endswith('_County')):
+        tag = tag.get('id')
+        tag = tag.encode('utf-8')
+        county_list.extend([tag])
+
+# For each county it pulls out each school name and city
 
 
 def schoolsearch():
@@ -33,17 +28,29 @@ def schoolsearch():
         county_ul = county_span.parent.find_next_sibling()
         for item in county_ul.findAll('li'):
             school = item.text.encode('utf-8')
-            school_list.extend([school])
+            school = school.split(',')
+            temp_list = []
+            school_name = school[0]
+            temp_list.extend([school_name])
+
+            # Accounts for schools that do not have a city listed
+            try:
+                city = school[1].strip()
+                temp_list.extend([city])
+            except IndexError:
+                temp_list.extend(["No city listed"])
+            school_list.extend([temp_list])
+
+# Saves data to a csv file with headers
 
 
 def savetofile():
     with open('schools.csv', 'wb') as results:
-        wr = csv.writer(results, dialect='excel')
-        wr.writerow(['School Name', 'City'])
-        for item in school_list:
-            wr.writerow([item])
+        writer = csv.writer(results)
+        writer.writerow(['School Name', 'City'])
+        writer.writerows(school_list)
     print 'The list has been printed to schools.csv'
 
-
+countysearch()
 schoolsearch()
 savetofile()
